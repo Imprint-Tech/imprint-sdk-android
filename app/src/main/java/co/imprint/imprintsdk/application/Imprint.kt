af9fb14b607ee27047dfb394ad327fd6cb5b8779
kaichang-imprint <kaichang@imprint.co>
2025-02-28 13:09:21 -0800

    Add comments

--- app/src/main/java/co/imprint/imprintsdk/application/Imprint.kt
+++ app/src/main/java/co/imprint/imprintsdk/application/Imprint.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.content.Intent
 import co.imprint.imprintsdk.application.ApplicationActivity.Companion.APPLICATION_CONFIGURATION
 
-/**
- * Main class for managing the Imprint application process.
- */
 object Imprint {
 
   /**
    * Starts the application process with the specified configuration.
    * @param context The context from which the application process will be presented.
    * @param configuration The configuration settings for the application process.
+   * @param onCompletion Callback to be invoked when the application process completes.
    */
   fun startApplication(
     context: Context,
--- app/src/main/java/co/imprint/imprintsdk/application/ImprintConfiguration.kt
+++ app/src/main/java/co/imprint/imprintsdk/application/ImprintConfiguration.kt
@@ -5,6 +5,11 @@ import kotlinx.parcelize.Parcelize
 
 /**
  * Configuration settings for the Imprint application process.
+ *
+ * @param clientSecret The client secret used to initiate the application session. Generated through Create Customer Session.
+ * Please refer to the API documentation (https://docs.imprint.co/api-reference/customer-sessions/create-a-new-customer-session) for details on obtaining a clientSecret.
+ * @param partnerReference The unique identifier for the program provided by the Imprint team.
+ * @param environment The environment for the application process. Default value is [Environment.PRODUCTION].
  */
 @Parcelize
 data class ImprintConfiguration(
@@ -22,6 +27,8 @@ data class ImprintConfiguration(
 
   /**
    * Terminal states for the application process.
+   *
+   * Possible values are [OFFER_ACCEPTED], [REJECTED], [ABANDONED], [ERROR].
    */
   enum class CompletionState {
     OFFER_ACCEPTED, REJECTED, ABANDONED, ERROR;
