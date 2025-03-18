a2deb0f20a079d630265053f16fa5e15b62ea195
JuanPabloSandin <qubika-juanpablo@imprint.co>
2025-03-18 16:42:44 -0300

    Fixes for building issues

--- sdk/src/main/java/co/imprint/sdk/presentation/ApplicationActivity.kt
+++ sdk/src/main/java/co/imprint/sdk/presentation/ApplicationActivity.kt
@@ -3,12 +3,12 @@ package co.imprint.sdk.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material3.MaterialTheme
 import co.imprint.sdk.di.dispatchersModule
 import co.imprint.sdk.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.context.startKoin
 
 internal class ApplicationActivity : ComponentActivity() {
@@ -29,7 +29,7 @@ internal class ApplicationActivity : ComponentActivity() {
       modules(dispatchersModule, viewModelModule)
     }
 
-    val viewModel: ApplicationViewModel by viewModels()
+    val viewModel: ApplicationViewModel by viewModel()
     setContent {
       MaterialTheme {
         ApplicationView(viewModel)
