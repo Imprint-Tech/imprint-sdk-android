cad832340c47f789419c03473db7b4f11a8a391b
kaichang-imprint <kaichang@imprint.co>
2025-04-05 23:20:55 -0700

    Tidy up

--- sdk/src/main/java/co/imprint/sdk/presentation/ApplicationActivity.kt
+++ sdk/src/main/java/co/imprint/sdk/presentation/ApplicationActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import androidx.lifecycle.SavedStateHandle
 import co.imprint.sdk.di.IsolatedKoinComponent
-import co.imprint.sdk.domain.repository.ImageRepository
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.parameter.parametersOf
 
 internal class ApplicationActivity : ComponentActivity(), IsolatedKoinComponent {
+
+  private val viewModel: ApplicationViewModel by viewModel()
 
   companion object {
     const val APPLICATION_CONFIGURATION = "config"
@@ -18,29 +17,6 @@ internal class ApplicationActivity : ComponentActivity(), IsolatedKoinComponent 
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    val stateMap = mutableMapOf<String, Any?>()
-    intent?.extras?.let { bundle ->
-      for (key in bundle.keySet()) {
-        stateMap[key] = bundle.get(key)
-      }
-    }
-    savedInstanceState?.let { bundle ->
-      for (key in bundle.keySet()) {
-        stateMap[key] = bundle.get(key)
-      }
-    }
-
-    // Create SavedStateHandle with the map
-    val state = SavedStateHandle(stateMap)
-
-    // Get the ImageRepository from your isolated Koin context
-    val imageRepository = getKoin().get<ImageRepository>()
-
-    // Provide both required parameters to the ViewModel
-    val viewModel: ApplicationViewModel by viewModel {
-      parametersOf(state, imageRepository)
-    }
 
     setContent {
       MaterialTheme {
