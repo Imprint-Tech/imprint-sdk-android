e994bc80cc52450c2114629b432a75d70cc6b81e
kaichang-imprint <kaichang@imprint.co>
2025-05-08 11:49:36 -0700

    Fix UT

--- sdk/src/test/java/co/imprint/sdk/presentation/utils/ExtensionsTest.kt
+++ sdk/src/test/java/co/imprint/sdk/presentation/utils/ExtensionsTest.kt
@@ -8,22 +8,24 @@ import org.junit.Test
 class ExtensionsTest {
 
   @Test
-  fun `toMapOrNull should return empty map when JSONObject is empty`() {
+  fun `toMap should return empty map when JSONObject is empty`() {
     val jsonObject = JSONObject()
     val result = jsonObject.toMap()
     assertEquals(emptyMap<String, Any?>(), result)
   }
 
   @Test
-  fun `toMapOrNull should return correct map when JSONObject has key-value pairs`() {
+  fun `toMap should return correct map when JSONObject has key-value pairs`() {
     val jsonObject = JSONObject().apply {
       put("key1", "value1")
       put("key2", "value2")
+      put("key3", 1)
     }
 
     val expectedMap = mapOf(
       "key1" to "value1",
-      "key2" to "value2"
+      "key2" to "value2",
+      "key3" to 1,
     )
 
     val result = jsonObject.toMap()
@@ -31,14 +33,14 @@ class ExtensionsTest {
   }
 
   @Test
-  fun `toMapOrNull should handle null values correctly`() {
+  fun `toMap should handle null values correctly`() {
     val jsonObject = JSONObject().apply {
       put("key1", JSONObject.NULL) // JSON stores null as JSONObject.NULL
       put("key2", "value2")
     }
 
     val expectedMap = mapOf(
-      "key1" to "", //Gets mapped as empty string
+      "key1" to null, //Gets mapped as empty string
       "key2" to "value2"
     )
 
